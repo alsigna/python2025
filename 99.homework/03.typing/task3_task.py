37edcc8fda82a81aa7d96d8cc394ac8f86b13a51
Alexander Ignatov <31988593+alsigna@users.noreply.github.com>
2025-06-20 11:45:43 +0300

    Delete 99.homework/03.typing/task3_task.py

--- 99.homework/03.typing/task3_task.py
+++
@@ -1,7 +0,0 @@
-# создать тип Email, и проверку, что адрес содержит @.
-# написать функцию send_email, которая принимает валидный Email
-
-if __name__ == "__main__":
-    send_email(validate_email("user@example.com"))  # ОК
-    send_email("user@example.com")  # mypy должен ругаться на строку, но в runtime все ОК
-    send_email("user-example.com")  # mypy должен ругаться на строку, но в runtime все ОК
