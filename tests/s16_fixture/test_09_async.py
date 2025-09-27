91ac005a955f394e329c52397d0ce29bda534716
alsigna <ignatov.alx@gmail.com>
2025-09-27 11:30:48 +0300

    interim

--- tests/s16_fixture/test_09_async.py
+++ tests/s16_fixture/test_09_async.py
@@ -10,23 +10,24 @@ from c18_pytest.src.s16_fixture.netbox_api_handler import NetboxAPIHandler
 
 @pytest.mark.api()
 class TestNetboxAPIHandler:
-    NETBOX_VERSION = "4.4.0"
+    NETBOX_VERSION = "4.4.1"
 
     @pytest.fixture()
-    def session(self) -> Iterator[httpx.Client]:
+    def session(self) -> Iterator[NetboxAPIHandler]:
         test_netbox_url = getenv("TEST_NETBOX_URL")
         test_netbox_token = getenv("TEST_NETBOX_TOKEN")
         with NetboxAPIHandler(test_netbox_url, test_netbox_token) as _session:
             yield _session
 
     @pytest.fixture()
-    async def asession(self) -> AsyncIterator[httpx.AsyncClient]:
+    async def asession(self) -> AsyncIterator[NetboxAPIHandler]:
         test_netbox_url = getenv("TEST_NETBOX_URL")
         test_netbox_token = getenv("TEST_NETBOX_TOKEN")
         async with NetboxAPIHandler(test_netbox_url, test_netbox_token) as _asession:
             yield _asession
 
-    def test_get(self, session: httpx.Client) -> None:
+    # @pytest.mark.xfail(reason="no netbox connection")
+    def test_get(self, session: NetboxAPIHandler) -> None:
         response = session.get("/api/status/")
 
         assert isinstance(response, list)
@@ -34,8 +35,9 @@ class TestNetboxAPIHandler:
         version = response[0].get("netbox-version")
         assert version == self.NETBOX_VERSION
 
-    @pytest.mark.asyncio()
-    async def test_aget_1(self, asession: httpx.AsyncClient) -> None:
+    # asyncio_mode = "auto" в настройках стоит, поэтому отдельно тесты можно не маркировать
+    # @pytest.mark.asyncio()
+    async def test_aget_1(self, asession: NetboxAPIHandler) -> None:
         loop = asyncio.get_running_loop()
         print(f"test_aget_1: {id(loop)=}")
 
@@ -46,8 +48,9 @@ class TestNetboxAPIHandler:
         version = response[0].get("netbox-version")
         assert version == self.NETBOX_VERSION
 
-    @pytest.mark.asyncio()
-    async def test_aget_2(self, asession: httpx.AsyncClient) -> None:
+    # asyncio_mode = "auto" в настройках стоит, поэтому отдельно тесты можно не маркировать
+    # @pytest.mark.asyncio()
+    async def test_aget_2(self, asession: NetboxAPIHandler) -> None:
         loop = asyncio.get_running_loop()
         print(f"test_aget_1: {id(loop)=}")
 
